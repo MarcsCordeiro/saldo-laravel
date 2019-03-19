0ea4c53aaac80b70e87825b968be2343ffd139ba
Marcos Cordeiro <vicentevieira300@gmail.com>
2019-03-19 14:41:06 -0300

    Projeto já iniciado

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            
+            'name'      => 'Maria Sooo',
+            'email'     => 'maria@laravel.com',
+            'password' => bcrypt('123456'),
+        ]);
+    }
+}
