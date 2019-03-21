4e9a7667770be032912f702b29192a3d3c98e2d9
Marcos Cordeiro <vicentevieira300@gmail.com>
2019-03-20 23:48:52 -0300

    Configurando historicos de recargas

--- app/Models/Balance.php
+++ app/Models/Balance.php
@@ -9,11 +9,19 @@ class Balance extends Model
     public $timestamps = false;
     
     public function deposit($value):array{
-        
+        $totalBefore = $this->amount ? $this->amount : 0;
         $this->amount += $value;
         $deposit = $this->save();
         
-        if($deposit)
+        $historic = auth()->user()->historics()->create([
+           'type'           => 'I',
+            'amount'        => $value, 
+            'total_before'  => $totalBefore, 
+            'total_after'   => $this->amount, 
+            'date'          => date('Ymd'),
+        ]);
+        
+        if($deposit && $historic)
             return[
             'success' => 'true',
             'message' => 'Sucesso ao carregar'
